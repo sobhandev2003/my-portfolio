f758d248c4a80ae2b89f449bc106aee91ae21a91
Sobhandev Pramanik <sobhandevp2021@gmail.com>
2024-02-26 03:00:08 +0530

    Creat navbar

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <section>
+      <Navbar/>
+      </section>
+      <section className='main-body'>
+
+      </section>
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+import '../css/Navbar.css'
+import { useEffect, useState } from "react";
+import navLogo from '../assets/navLogo.png'
+import { Link } from "react-scroll";
+import MenuIcon from '@mui/icons-material/Menu';
+
+const NavItems = ["Home", "About", "Skill", "Education", "Contact"]
+const Navbar: React.FC = () => {
+    const [isMobile, setIsMobile] = useState(false);
+    const [isShowDialog,setIsShowDialog]=useState(false)
+    const handleLinkClick = () => {
+        isMobile && setIsShowDialog(false); // Set isShowDialog to false when any link is clicked
+      };
+    const navLinks = (
+        NavItems.map((items) => {
+            return <Link
+                activeClass="active"
+                to={`${items.toLowerCase()}`}
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={500}
+                onClick={handleLinkClick}
+            >
+                {items}
+            </Link>
+        })
+    )
+    useEffect(() => {
+        const mobileQuery = window.matchMedia('(max-width: 768px)');
+        setIsMobile(mobileQuery.matches)
+        const handleResize = (): void => {
+            setIsMobile(mobileQuery.matches);
+        };
+
+        mobileQuery.addEventListener('change', handleResize)
+        return () => {
+            // Remove event listener
+            mobileQuery.removeEventListener('change', handleResize);
+        };
+    }, [])
+    
+    return (
+        <nav className="nav">
+            <section>
+                <img src={navLogo} alt="" className="nav-logo" />
+                <h3>Sobhan</h3>
+            </section>
+            <section>
+                {!isMobile && navLinks}
+                {isMobile &&  <MenuIcon className='toggle-btn' onClick={():void=>setIsShowDialog(!isShowDialog)}/>}
+            </section>
+            {
+               isMobile && isShowDialog &&<div className='mobile-nav-items-dialog'>
+                  {navLinks}
+                </div>
+            }
+
+        </nav>
+
+    )
+}
+
+export default Navbar
