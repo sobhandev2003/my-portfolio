98291fcbb9fc683a3cc6a6f385c5dd4a1c5a9105
Sobhandev Pramanik <sobhandevp2021@gmail.com>
2024-03-01 03:56:13 +0530

    create contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,41 @@
 import '../css/Contact.css'
-
+import contactPage from '../assets/contactPage.jpeg';
+import { TfiHeadphoneAlt } from "react-icons/tfi";
+import { FaUser } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { BsFillSendFill } from "react-icons/bs";
+import { BiMessageDetail } from "react-icons/bi";
 const Contact:React.FC = () => {
-  return <div id='contact'></div>
+  return <div id='contact'>
+    <h1><span><TfiHeadphoneAlt /></span>Get In<span className='color-6200ee'> Touch</span></h1>
+  <div className='contact-box'>
+  <section className='image-section'>
+      <img src={contactPage} alt=''></img>
+    </section>
+    <section className='contact-section'>
+      <form className='contact-from'>
+        <label >
+        <input type="text" className='input-field' name="name" minLength={3} maxLength={50} placeholder='' required />
+        <span className='placeholder'><FaUser />Name </span>
+      </label>
+      <label >
+      <input type="email" className='input-field' name="email" placeholder=''  maxLength={20} required/>
+      <span className='placeholder'><MdEmail />Email</span>
+      </label>
+    <label >
+    <input type="tel" className='input-field' name="phone" placeholder='' pattern="[0-9]{10}" required/>
+    <span className='placeholder'><FaPhoneAlt />Phone</span>
+    </label>
+    <label >
+    <textarea name="message" className='textarea-field'  placeholder='' minLength={10}  required spellCheck={true}/>
+    <span className='textarea-placeholder'><BiMessageDetail />Message</span>
+    </label>
+      <button type="submit" className='send-btn'>Send<BsFillSendFill /></button>
+      </form>
+    </section>
+  </div>
+  </div>
 }
 
 export default Contact
--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,6 +1,17 @@
 import'../css/Education.css'
 import {education  } from "../jsonFiles/Education.json";
 const Education:React.FC= () => {
+  function capitalize(str: string): string {
+    if (!str) {
+      return str;
+    }
+    const words=str.split(' ');
+    let newStr=""
+    words.forEach((word)=>{
+        newStr=newStr+" "+word.charAt(0).toUpperCase()+word.slice(1).toLowerCase();
+    })
+    return newStr;
+  }
 const educationsTemplates=(education.map((courestion,index)=>{
   const {CourseTitle,instituteName,instituteAddress,instituteImage,Duration,Status}=courestion;
   return <div className='courses-div' key={index}>
@@ -10,7 +21,7 @@ const educationsTemplates=(education.map((courestion,index)=>{
   <section className='courses-details'>
     <h1 className='courses-title'>{CourseTitle}</h1>
     <p className='institute'>
-      <span className='institute-name'>{instituteName}</span>
+      <span className='institute-name'>{capitalize(instituteName)}</span>
     <span className='institute-address'> {instituteAddress}</span>
     </p>
     <p className='courses-status'> {Duration}|{Status}</p>
