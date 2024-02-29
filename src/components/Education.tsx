3d64184150d0e0648a9df9254dd0a24581174ae3
Sobhandev Pramanik <sobhandevp2021@gmail.com>
2024-03-01 01:02:34 +0530

    Add Education details

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,7 +1,25 @@
 import'../css/Education.css'
-
+import {education  } from "../jsonFiles/Education.json";
 const Education:React.FC= () => {
-  return <div id="education"></div>
+const educationsTemplates=(education.map((courestion,index)=>{
+  const {CourseTitle,instituteName,instituteAddress,instituteImage,Duration,Status}=courestion;
+  return <div className='courses-div' key={index}>
+  <section className='institute-image'>
+    <img src={`education/${instituteImage}`} alt=''></img>
+    </section>
+  <section className='courses-details'>
+    <h1 className='courses-title'>{CourseTitle}</h1>
+    <p className='institute'>
+      <span className='institute-name'>{instituteName}</span>
+    <span className='institute-address'> {instituteAddress}</span>
+    </p>
+    <p className='courses-status'> {Duration}|{Status}</p>
+  </section>
+</div>
+}))
+  return <div id="education">
+    {educationsTemplates}
+  </div>
 }
 
 export default Education
