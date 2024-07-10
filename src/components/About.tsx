ea4c4c44ff7f3c5bf4a9657164b9d8acda1e4faa
sobhandev2003 <sobhandevp2021@gmail.com>
2024-07-10 11:44:10 +0530

    Update About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -34,7 +34,7 @@ const About: React.FC = () => {
     <section>
       <h2 >I'm Sobhandev</h2>
       <h3>Full Stack Developer</h3>
-      <p> I am Sobhandev Pramanik, a full-stack developer from Arambag, West Bengal, India. I'm currently studying Computer Science and Engineering at Abacus Institute of Engineering and Management. I really enjoy coding and solving problems. My expertise lies in MERN stack development, where I create web solutions from the front to the back end. I love the excitement of making cool and user-friendly applications that matter.</p>
+      <p> I am currently pursuing a degree in Computer Science and Engineering at the Abacus Institute of Engineering and Management, affiliated with MAKAUT. I will be graduating in 2025. I possess a solid foundation in front-end technologies, including HTML, CSS, JavaScript, TypeScript, React JS, and ReduxToolkit. My academic projects and personal endeavors have equipped me with hands-on experience in web development. For instance, I have successfully developed several websites and web applications, which have helped me improve my skills in creating responsive and user-friendly interfaces. Additionally, I have a strong understanding of back-end technologies including Node JS, Express JS, and MongoDB, MySQL as a database. I am proficient in Java, DSA, and Python.</p>
       <p><span className='color-02aabd'>Email </span>: <Link to={`mailto:${email}`} >{email}</Link></p>
       <p><><span className='color-02aabd'>Address</span>: <span >West Bengal , India </span></></p>
       <Link to={resumeUrl} target='_blank' className='resume-download-btn' > Resume <MdArrowForwardIos className='icon' /></Link>
