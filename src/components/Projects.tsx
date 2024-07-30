e013e358f339f815b7b04b1a25d04658dfa27f84
sobhandev2003 <sobhandevp2021@gmail.com>
2024-07-30 20:28:35 +0530

    Add project section slading animation

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,21 +1,39 @@
 import '../css/Projects.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { ProjectsCard } from './Cards'
 import projects from '../jsonFiles/Projects.json'
+import Slider from "react-slick";
 const Projects = () => {
-  return <div id="projects">
-    {
-      projects && projects.projects.map((project,index)=>{
-        return <ProjectsCard key={index}
-        title={project.title}
-        description={project.description}
-        snapshot={project.snapshot}
-        githubRepoLink={project.githubRepoLink}
-        websiteLink={project.websiteLink}
-        tools={project.tools}
-        />
-      })
-    }
-    
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    cssEase: "linear"
+  };
+  return <div id="projects" className=''>
+    <div className=' w-[45%]'>
+      {
+        projects &&
+        <Slider {...settings} >{
+          projects.projects.map((project, index) => {
+            return <ProjectsCard key={index}
+              title={project.title}
+              description={project.description}
+              snapshot={project.snapshot}
+              githubRepoLink={project.githubRepoLink}
+              websiteLink={project.websiteLink}
+              tools={project.tools}
+            />
+          })
+        }
+        </Slider>
+      }
+    </div>
   </div>
 }
 
